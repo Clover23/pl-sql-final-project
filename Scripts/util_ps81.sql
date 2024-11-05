0a76a050886da1ac0afa82ced43ded5d40f01c33
Kateryna Shlapatska <Catrinash8@gmail.com>
2024-11-05 12:15:31 +0200

    Create util_ps81.sql
    Check package util for PS-81 task

---
+++ Scripts/util_ps81.sql
@@ -0,0 +1,55 @@
+
+--Правильні дані
+BEGIN
+util.add_employee(p_first_name => 'John',
+                                p_last_name => 'Smith',
+                                p_email => 'JOHNSMITH',
+                                p_phone_number => '5678945321',
+                                p_job_id => 'AD_PRES',
+                                p_salary => 21000,
+                                p_department_id => 10
+                                );
+END;
+
+--Неіснуючий job_id
+BEGIN                                                                
+util.add_employee(p_first_name => 'John',
+                                p_last_name => 'Smith',
+                                p_email => 'JOHNSMITH',
+                                p_phone_number => '5678945321',
+                                p_job_id => 'AD_PRESS',
+                                p_salary => 21000,
+                                p_department_id => 10
+                                );
+END;
+
+--Не коректна зарплата
+BEGIN
+util.add_employee(p_first_name => 'John',
+                                p_last_name => 'Smith',
+                                p_email => 'JOHNSMITH',
+                                p_phone_number => '5678945321',
+                                p_job_id => 'AD_PRES',
+                                p_salary => 1000,
+                                p_department_id => 10
+                                );
+END;
+
+--Не коректний department_id
+BEGIN                               
+util.add_employee(p_first_name => 'John',
+                                p_last_name => 'Smith',
+                                p_email => 'JOHNSMITH',
+                                p_phone_number => '567.894.5321',
+                                p_job_id => 'AD_PRES',
+                                p_salary => 21000,
+                                p_department_id => 1000
+                                );
+END;
+
+
+
+
+SELECT * FROM logs ORDER BY log_date DESC;
+
+SELECT * FROM employees ORDER BY hire_date DESC;
