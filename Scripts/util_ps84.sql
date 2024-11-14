942ae8dae2dbea6155b884ec0e02ab6c74bc4424
Kateryna Shlapatska <Catrinash8@gmail.com>
2024-11-14 19:08:31 +0200

    Create util_ps84.sql

---
+++ Scripts/util_ps84.sql
@@ -0,0 +1,18 @@
+
+DECLARE
+result VARCHAR2(100);
+BEGIN
+    util.copy_table(p_source_scheme => 'HR' 
+                       , p_target_scheme => 'KATERINA_JA5'
+                       , p_list_table => 'COUNTRIES,PRODUCTS_OLD,CANDIES'
+                       , p_copy_data => FALSE
+                       , po_result => result);
+END;
+
+SELECT * FROM COUNTRIES;
+SELECT * FROM products_old;
+
+DROP TABLE countries;
+DROP TABLE products_old;
+
+SELECT * FROM LOGS ORDER BY LOG_DATE DESC;
