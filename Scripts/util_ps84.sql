91abfdd4705b28cdc038dffe55f4aa7dd5d3b54e
Kateryna Shlapatska <Catrinash8@gmail.com>
2024-11-15 00:57:49 +0200

    Update util_ps84.sql

--- Scripts/util_ps84.sql
+++ Scripts/util_ps84.sql
@@ -3,7 +3,7 @@ result VARCHAR2(100);
 BEGIN
     util.copy_table(p_source_scheme => 'HR' 
                        --, p_target_scheme => 'KATERINA_JA5'
-                       , p_list_table => 'COUNTRIES,PRODUCTS_OLD,CANDIES'
+                       , p_list_table => 'countries,PRODUCTS_OLD,CANDIES'
                        , p_copy_data => FALSE
                        , po_result => result);
     dbms_output.put_line(result);
@@ -15,4 +15,4 @@ SELECT * FROM products_old;
 DROP TABLE countries;
 DROP TABLE products_old;
 
-SELECT * FROM LOGS ORDER BY LOG_DATE DESC;
+SELECT * FROM LOGS ORDER BY ID DESC;
